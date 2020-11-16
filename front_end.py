2a528d7e1f2e423008e76851f034536625c2d11e
vishwesh-19 <59824536+vishwesh-19@users.noreply.github.com>
2020-11-16 19:28:36 +0530

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox as mb
@@ -16,8 +17,16 @@ l_name_1 = Entry(window).grid(row = 1,column = 1)                               
 e-mail_1 = Entry(window).grid(row = 2,column = 1)                                   # Text widget for e-mail
 contact_no = Entry(window).grid(row = 3,column = 1)                                 # Text widget for contact no.
 
+print("How can we help you?")
+
+list_of_problems = ["Food","Medicine","Fuel for cooking","Migration","Others"]
+problem = Tkinter.StringVar(window)
+
+list_widget = tk.OptionMenu(window ,problem, list_of_problems)
+list_widget.pack()
 def clicked():
    msg = msb.showinfo("button clicked" , "We will try to help you as soon as possible ")
  
 btn = ttk.Button(window ,text="Seek Help" ,command="clicked()").grid(row=4,column=0)
+btn.pack()
 window.mainloop()
